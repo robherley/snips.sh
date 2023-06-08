4b2c8df440e53b9af88bc8619bbcc3e17f3d8cfe
Rob Herley <robherley@github.com>
2023-06-08 12:16:15 -0400

    update after merge

--- internal/testutil/fixtures.go
+++ internal/testutil/fixtures.go
@@ -12,13 +12,16 @@ type fixtures struct{}
 var Fixtures = &fixtures{}
 
 func (f *fixtures) File() snips.File {
-	return snips.File{
+	file := snips.File{
 		ID:        id.New(),
 		CreatedAt: time.Now().Add(-5 * time.Minute),
 		UpdatedAt: time.Now().Add(-5 * time.Minute),
 		Size:      100,
-		Content:   []byte("hello world"), // TODO: update with main
 		Private:   false,
 		UserID:    id.New(),
 	}
+
+	file.SetContent([]byte("hello world"), true)
+
+	return file
 }
