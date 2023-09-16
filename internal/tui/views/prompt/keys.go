6f7b5661ed2de54c4006e333a6f64a3f9d8559d3
Rob Herley <robherley@github.com>
2023-09-16 13:59:01 -0400

    hide 'enter' keybind after prompt is submitted

--- internal/tui/views/prompt/keys.go
+++ internal/tui/views/prompt/keys.go
@@ -3,6 +3,8 @@ package prompt
 import "github.com/charmbracelet/bubbles/key"
 
 type keyMap struct {
+	submitted bool
+
 	Help   key.Binding
 	Quit   key.Binding
 	Enter  key.Binding
@@ -10,31 +12,48 @@ type keyMap struct {
 }
 
 func (km keyMap) ShortHelp() []key.Binding {
-	return []key.Binding{km.Help, km.Escape, km.Enter}
+	bindings := []key.Binding{km.Help, km.Escape}
+
+	if !km.submitted {
+		bindings = append(bindings, km.Enter)
+	}
+
+	return bindings
 }
 
 func (km keyMap) FullHelp() [][]key.Binding {
-	return [][]key.Binding{
-		{km.Escape, km.Enter},
+	bindings := [][]key.Binding{
+		{km.Escape},
 		{km.Help, km.Quit},
 	}
+
+	if !km.submitted {
+		bindings[0] = append(bindings[0], km.Enter)
+	}
+
+	return bindings
 }
 
-var keys = keyMap{
-	Help: key.NewBinding(
-		key.WithKeys("?"),
-		key.WithHelp("?", "help"),
-	),
-	Enter: key.NewBinding(
-		key.WithKeys("enter"),
-		key.WithHelp("enter", "submit"),
-	),
-	Escape: key.NewBinding(
-		key.WithKeys("esc"),
-		key.WithHelp("esc", "go back"),
-	),
-	Quit: key.NewBinding(
-		key.WithKeys("q", "ctrl+c"),
-		key.WithHelp("q", "quit"),
-	),
+func newKeyMap(submitted bool) keyMap {
+	km := keyMap{
+		Help: key.NewBinding(
+			key.WithKeys("?"),
+			key.WithHelp("?", "help"),
+		),
+		Enter: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "submit"),
+		),
+		Escape: key.NewBinding(
+			key.WithKeys("esc"),
+			key.WithHelp("esc", "go back"),
+		),
+		Quit: key.NewBinding(
+			key.WithKeys("q", "ctrl+c"),
+			key.WithHelp("q", "quit"),
+		),
+	}
+
+	km.submitted = submitted
+	return km
 }
